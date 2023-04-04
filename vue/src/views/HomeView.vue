77ca03e7f1fd7156c27e5234bc6d21f472497259
AngelaW45 <113141296+AngelaW45@users.noreply.github.com>
2023-04-04 09:59:29 -0400

    3

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -5,13 +5,13 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 const complaints = ref('')
-async function getdata() {
+async function getData() {
   let res = await fetch('https://data.cityofnewyork.us/resource/nre2-6m2s.json')
-  let Data = res.json
+  let Data = await res.json()
   complaints.value = Data.results
 }
 onMounted(() => {
-  getdata()
+  getData()
 })
 </script>
 
