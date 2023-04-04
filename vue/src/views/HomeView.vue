8931c4ca8bd9649feaf00c53697bee8371c1c0c2
AngelaW45 <113141296+AngelaW45@users.noreply.github.com>
2023-04-04 09:54:17 -0400

    2

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,9 +1,18 @@
+<template>
+  <div></div>
+</template>
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref, onMounted } from 'vue'
+const complaints = ref('')
+async function getdata() {
+  let res = await fetch('https://data.cityofnewyork.us/resource/nre2-6m2s.json')
+  let Data = res.json
+  complaints.value = Data.results
+}
+onMounted(() => {
+  getdata()
+})
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped></style>
