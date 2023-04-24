dec9597c3b5a9ca019982534487f5604a3dfd660
AngelaW45 <113141296+AngelaW45@users.noreply.github.com>
2023-04-24 09:43:48 -0400

    7

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
 // import { RouterLink, RouterView } from 'vue-router'
 // import HelloWorld from './components/HelloWorld.vue'
-
 import BarChart from './components/BarChart.vue'
 </script>
 
