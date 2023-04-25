4ec80f068bd272f449a38f604f26d59e166f181a
AngelaW45 <113141296+AngelaW45@users.noreply.github.com>
2023-04-25 09:51:55 -0400

    9

--- vue/src/components/BarChart.vue
+++ vue/src/components/BarChart.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="container">
-    <Bar v-if="loaded" :data="chartData" />
-  </div>
+  <Bar id="my-chart-id" :options="chartOptions" :data="chartData" />
 </template>
 
 <script>
@@ -19,147 +17,78 @@ import {
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
 export default {
+  props: {
+    label: {
+      type: Arry
+    },
+    chartData: {
+      type: Array
+    }
+  },
   name: 'BarChart',
   components: { Bar },
-  data: () => ({
-    loaded: false,
-    chartData: {
-      labels: [
-        'AL',
-        'AK',
-        'AZ',
-        'AR',
-        'CA',
-        'CO',
-        'CT',
-        'DE',
-        'FL',
-        'GA',
-        'HI',
-        'ID',
-        'IL',
-        'IN',
-        'IA',
-        'KS',
-        'KY',
-        'LA',
-        'ME',
-        'MD',
-        'MA',
-        'MI',
-        'MN',
-        'MS',
-        'MO',
-        'MT',
-        'NE',
-        'NV',
-        'NH',
-        'NT',
-        'NM',
-        'NY',
-        'NC',
-        'ND',
-        'OK',
-        'OR',
-        'PA',
-        'RI',
-        'SC',
-        'SD',
-        'TN',
-        'TX',
-        'UT',
-        'VT',
-        'VA',
-        'WA',
-        'WV',
-        'WI',
-        'WY',
-        'N/A'
-      ],
-      datasets: [
-        {
-          data: []
-        }
-      ]
-    },
-    chartOptions: {
-      responsive: true
-    }
-  }),
-
-  import { ref, onMounted } from 'vue'
-const complaints = ref('')
-async function getData() {
-  let res = await fetch('https://data.cityofnewyork.us/resource/nre2-6m2s.json')
-  let data = await res.json()
-  complaints.value = await data
-  //console.log(JSON.stringify(data))
-}
-onMounted(() => {
-  getData()
-  //console.log(getData())
-})
-}
-</script>
-
-<!-- return {
+  data() {
+    return {
       chartData: {
         labels: [
-          'Alabama',
-          'Alaska',
-          'Arizona',
-          'Arkansas',
-          'California',
-          'Colorado',
-          'Connecticut',
-          'Delaware',
-          'Florida',
-          'Georgia',
-          'Hawaii',
-          'Idaho',
-          'Illinois',
-          'Indiana',
-          'Iowa',
-          'Kansas',
-          'Kentucky',
-          'Louisiana',
-          'Maine',
-          'Maryland',
-          'Massachusetts',
-          'Michigan',
-          'Minnesota',
-          'Mississippi',
-          'Missouri',
-          'Montana',
-          'Nebraska',
-          'Nevada',
-          'New Hampshire',
-          'New Jersey',
-          'New Mexico',
-          'New York',
-          'North Carolina',
-          'North Dakota',
-          'Ohio',
-          'Oklahoma',
-          'Oregon',
-          'Pennsylvania',
-          'Rhode Island',
-          'South Carolina',
-          'South Dakota',
-          'Tennessee',
-          'Texas',
-          'Utah',
-          'Vermont',
-          'Virginia',
-          'Washington',
-          'West Virginia',
-          'Wisconsin',
-          'Wyoming',
+          'AL',
+          'AK',
+          'AZ',
+          'AR',
+          'CA',
+          'CO',
+          'CT',
+          'DE',
+          'FL',
+          'GA',
+          'HI',
+          'ID',
+          'IL',
+          'IN',
+          'IA',
+          'KS',
+          'KY',
+          'LA',
+          'ME',
+          'MD',
+          'MA',
+          'MI',
+          'MN',
+          'MS',
+          'MO',
+          'MT',
+          'NE',
+          'NV',
+          'NH',
+          'NT',
+          'NM',
+          'NY',
+          'NC',
+          'ND',
+          'OK',
+          'OR',
+          'PA',
+          'RI',
+          'SC',
+          'SD',
+          'TN',
+          'TX',
+          'UT',
+          'VT',
+          'VA',
+          'WA',
+          'WV',
+          'WI',
+          'WY',
           'N/A'
         ],
-        datasets: [{ data: [40, 20, 12] }]
-
-
+        datasets: [
+          {
+            label: 'COMPLAINT CASES',
+            backgroundColor: ['#E6F7FF'],
+            data: this.business_state
+          }
+        ]
       },
       chartOptions: {
         responsive: true
@@ -167,4 +96,4 @@ onMounted(() => {
     }
   }
 }
-</script> -->
+</script>
