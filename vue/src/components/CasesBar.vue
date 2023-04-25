586a408f5fbe8b23dda272eb36fc1e517399552b
AngelaW45 <113141296+AngelaW45@users.noreply.github.com>
2023-04-25 10:13:05 -0400

    10

--- vue/src/components/CasesBar.vue
+++ vue/src/components/CasesBar.vue
@@ -2,7 +2,17 @@
   <Bar id="my-chart-id" :options="chartOptions" :data="chartData" />
 </template>
 
-<script>
+<script setup>
+import { ref, onMounted } from 'vue'
+const complaints = ref('')
+async function getData() {
+  let res = await fetch('https://data.cityofnewyork.us/resource/nre2-6m2s.json')
+  let data = await res.json()
+  complaints.value = await data
+  //console.log(JSON.stringify(data))
+},
+
+
 import { Bar } from 'vue-chartjs'
 import {
   Chart as ChartJS,
@@ -17,14 +27,6 @@ import {
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
 export default {
-  props: {
-    label: {
-      type: Arry
-    },
-    chartData: {
-      type: Array
-    }
-  },
   name: 'BarChart',
   components: { Bar },
   data() {
@@ -96,4 +98,74 @@ export default {
     }
   }
 }
+onMounted(() => {
+  getData()
+  //console.log(getData())
+}),
 </script>
+
+<!-- name: 'BarChart',
+  components: { Bar },
+  data() {
+    return {
+      chartData: {
+        labels: [
+          'AL',
+          'AK',
+          'AZ',
+          'AR',
+          'CA',
+          'CO',
+          'CT',
+          'DE',
+          'FL',
+          'GA',
+          'HI',
+          'ID',
+          'IL',
+          'IN',
+          'IA',
+          'KS',
+          'KY',
+          'LA',
+          'ME',
+          'MD',
+          'MA',
+          'MI',
+          'MN',
+          'MS',
+          'MO',
+          'MT',
+          'NE',
+          'NV',
+          'NH',
+          'NT',
+          'NM',
+          'NY',
+          'NC',
+          'ND',
+          'OK',
+          'OR',
+          'PA',
+          'RI',
+          'SC',
+          'SD',
+          'TN',
+          'TX',
+          'UT',
+          'VT',
+          'VA',
+          'WA',
+          'WV',
+          'WI',
+          'WY',
+          'N/A'
+        ], 
+
+      chartOptions: {
+        responsive: true
+      }
+    }
+  }
+}
+</script> -->
