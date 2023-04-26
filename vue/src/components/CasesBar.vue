e9d3f0d3f6c6af8b8289e3b9e7bdad192c0d320a
AngelaW45 <113141296+AngelaW45@users.noreply.github.com>
2023-04-26 10:12:18 -0400

    11

--- vue/src/components/CasesBar.vue
+++ vue/src/components/CasesBar.vue
@@ -1,17 +1,23 @@
 <template>
-  <Bar id="my-chart-id" :options="chartOptions" :data="chartData" />
+  <Bar id="my-chart-id" :options="chartOptions" v-if="loaded" :data="chartData" />
 </template>
 
-<script setup>
-import { ref, onMounted } from 'vue'
-const complaints = ref('')
-async function getData() {
-  let res = await fetch('https://data.cityofnewyork.us/resource/nre2-6m2s.json')
-  let data = await res.json()
-  complaints.value = await data
-  //console.log(JSON.stringify(data))
-},
+<script>
+{
+  /* <script setup>
+// import { ref, onMounted } from 'vue'
+// const complaints = ref('')
+// async function getData() {
+//   let res = await fetch('https://data.cityofnewyork.us/resource/nre2-6m2s.json')
+//   let data = await res.json()
+//   complaints.value = await data
+//   //console.log(JSON.stringify(data))
+// }
 
+// onMounted(() => {
+//   getData()
+// }) */
+}
 
 import { Bar } from 'vue-chartjs'
 import {
@@ -31,77 +37,82 @@ export default {
   components: { Bar },
   data() {
     return {
-      chartData: {
-        labels: [
-          'AL',
-          'AK',
-          'AZ',
-          'AR',
-          'CA',
-          'CO',
-          'CT',
-          'DE',
-          'FL',
-          'GA',
-          'HI',
-          'ID',
-          'IL',
-          'IN',
-          'IA',
-          'KS',
-          'KY',
-          'LA',
-          'ME',
-          'MD',
-          'MA',
-          'MI',
-          'MN',
-          'MS',
-          'MO',
-          'MT',
-          'NE',
-          'NV',
-          'NH',
-          'NT',
-          'NM',
-          'NY',
-          'NC',
-          'ND',
-          'OK',
-          'OR',
-          'PA',
-          'RI',
-          'SC',
-          'SD',
-          'TN',
-          'TX',
-          'UT',
-          'VT',
-          'VA',
-          'WA',
-          'WV',
-          'WI',
-          'WY',
-          'N/A'
-        ],
-        datasets: [
-          {
-            label: 'COMPLAINT CASES',
-            backgroundColor: ['#E6F7FF'],
-            data: this.business_state
-          }
-        ]
-      },
-      chartOptions: {
-        responsive: true
-      }
+      chartData: null,
+      chartOptions: null,
+      loaded: false
     }
-  }
+  },
+
+  mounted: function () {
+    let x = ['3', '5', '6']
+    this.chartData = {
+      labels: [
+        'AL',
+        'AK',
+        'AZ',
+        'AR',
+        'CA',
+        'CO',
+        'CT',
+        'DE',
+        'FL',
+        'GA',
+        'HI',
+        'ID',
+        'IL',
+        'IN',
+        'IA',
+        'KS',
+        'KY',
+        'LA',
+        'ME',
+        'MD',
+        'MA',
+        'MI',
+        'MN',
+        'MS',
+        'MO',
+        'MT',
+        'NE',
+        'NV',
+        'NH',
+        'NT',
+        'NM',
+        'NY',
+        'NC',
+        'ND',
+        'OK',
+        'OR',
+        'PA',
+        'RI',
+        'SC',
+        'SD',
+        'TN',
+        'TX',
+        'UT',
+        'VT',
+        'VA',
+        'WA',
+        'WV',
+        'WI',
+        'WY',
+        'N/A'
+      ],
+
+      datasets: [
+        {
+          label: 'COMPLAINT CASES',
+          backgroundColor: ['#E6F7FF'],
+          data: x
+        }
+      ]
+    }
+  },
+  this.chartOptions: {
+    responsive: true
+  },
+  loaded: true
 }
-onMounted(() => {
-  getData()
-  //console.log(getData())
-}),
 </script>
 
 <!-- name: 'BarChart',
