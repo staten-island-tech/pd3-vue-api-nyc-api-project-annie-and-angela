09f8d75bc5ae564bc0d7061902bb443b085fb7c3
AngelaW45 <113141296+AngelaW45@users.noreply.github.com>
2023-04-29 16:26:28 -0400

    13 (home)

--- vue/src/components/BarChart.vue
+++ vue/src/components/BarChart.vue
@@ -21,108 +21,6 @@ ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 export default {
   name: 'BarChart',
   components: { Bar },
-  data: () => ({
-    // return: {
-    chartData: null,
-    chartOptions: null,
-    loaded: false
-    // }
-  }),
-
-  async mounted() {
-    this.loaded = false
-
-    let x = ['3', '5', '6']
-    this.chartData = {
-      labels: [
-        'AL',
-        'AK',
-        'AZ',
-        'AR',
-        'CA',
-        'CO',
-        'CT',
-        'DE',
-        'FL',
-        'GA',
-        'HI',
-        'ID',
-        'IL',
-        'IN',
-        'IA',
-        'KS',
-        'KY',
-        'LA',
-        'ME',
-        'MD',
-        'MA',
-        'MI',
-        'MN',
-        'MS',
-        'MO',
-        'MT',
-        'NE',
-        'NV',
-        'NH',
-        'NT',
-        'NM',
-        'NY',
-        'NC',
-        'ND',
-        'OK',
-        'OR',
-        'PA',
-        'RI',
-        'SC',
-        'SD',
-        'TN',
-        'TX',
-        'UT',
-        'VT',
-        'VA',
-        'WA',
-        'WV',
-        'WI',
-        'WY',
-        'N/A'
-      ],
-      datasets: [
-        {
-          label: 'COMPLAINT CASES',
-          backgroundColor: ['#E6F7FF'],
-          data: x
-        }
-      ]
-    }
-  },
-  chartOptions: {
-    responsive: true,
-    loaded: true
-  }
-}
-</script>
-
-<!-- <template>
-  <Bar id="my-chart-id" :options="chartOptions" v-if="loaded" :data="chartData" />
-</template>
-
-<script>
-import { Bar } from 'vue-chartjs'
-import {
-  Chart as ChartJS,
-  Title,
-  Tooltip,
-  Legend,
-  BarElement,
-  CategoryScale,
-  LinearScale
-} from 'chart.js'
-
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
-
-export default {
-  name: 'BarChart',
-  components: { Bar },
   data() {
     return {
       chartData: null,
@@ -131,7 +29,18 @@ export default {
     }
   },
 
-  mounted: function () {
+  Mounted: function () {
+    /* async function DATA() {
+      const wait = await fetch('https://data.cityofnewyork.us/resource/nre2-6m2s.json')
+      const data = await wait.json()
+      const dataArray = []
+      dataArray.push(data)
+      console.log(dataArray)
+      const text = dataArray.filter((e) => e.satisfaction === 'No').length
+      console.log(text)
+    }
+    DATA() */
+
     let x = ['3', '5', '6']
     this.chartData = {
       labels: [
@@ -186,7 +95,6 @@ export default {
         'WY',
         'N/A'
       ],
-
       datasets: [
         {
           label: 'COMPLAINT CASES',
@@ -195,12 +103,10 @@ export default {
         }
       ]
     }
-  },
-  chartOptions: {
-    responsive: true
-  },
-  loaded: true
+    this.chartOptions = {
+      responsive: true
+    }
+    this.loaded = true
+  }
 }
 </script>
-}
- -->
