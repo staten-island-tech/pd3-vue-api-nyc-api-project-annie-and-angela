73671c3449283b6918bc305d39f55b0c59ad221a
AngelaW45 <113141296+AngelaW45@users.noreply.github.com>
2023-04-29 23:12:37 -0400

    17 (Chart Appears with null)

--- vue/src/components/BarChart.vue
+++ vue/src/components/BarChart.vue
@@ -1,5 +1,5 @@
 <template>
-  <Bar id="my-chart-id" :options="chartOptions" :data="chartData" />
+  <Bar v-if="loaded" id="my-chart-id" :options="chartOptions" :data="chartData" />
 </template>
 
 <script>
@@ -16,120 +16,28 @@ import {
 
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
-export default {
-  name: 'BarChart',
-  components: { Bar },
-  data() {
-    return {
-      chartData: {
-        labels: [
-          'AL',
-          'AK',
-          'AZ',
-          'AR',
-          'CA',
-          'CO',
-          'CT',
-          'DE',
-          'FL',
-          'GA',
-          'HI',
-          'ID',
-          'IL',
-          'IN',
-          'IA',
-          'KS',
-          'KY',
-          'LA',
-          'ME',
-          'MD',
-          'MA',
-          'MI',
-          'MN',
-          'MS',
-          'MO',
-          'MT',
-          'NE',
-          'NV',
-          'NH',
-          'NT',
-          'NM',
-          'NY',
-          'NC',
-          'ND',
-          'OK',
-          'OR',
-          'PA',
-          'RI',
-          'SC',
-          'SD',
-          'TN',
-          'TX',
-          'UT',
-          'VT',
-          'VA',
-          'WA',
-          'WV',
-          'WI',
-          'WY',
-          'N/A'
-        ],
-        datasets: [
-          {
-            label: 'COMPLAINT CASES',
-            backgroundColor: ['#E6F7FF'],
-            data: [20, 20, 20]
-          }
-        ]
-      },
-      chartOptions: {
-        responsive: true
-      }
-    }
-  }
-}
-</script>
-
-<!-- <template>
-  <h1>Bar Chart</h1>
-  <div class="container">
-    <Bar id="my-chart-id" v-if="loaded" :data="chartData" :options="chartOptions" />
-  </div>
-</template>
-
-<script>
-import { Bar } from 'vue-chartjs'
-import {
-  Chart as ChartJS,
-  Title,
-  Tooltip,
-  Legend,
-  BarElement,
-  CategoryScale,
-  LinearScale
-} from 'chart.js'
-
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+/* const url = 'https://data.cityofnewyork.us/resource/nre2-6m2s.json' */
 
 export default {
   name: 'BarChart',
   components: { Bar },
   data() {
     return {
+      loaded: false,
       chartData: null,
-      chartOptions: null,
-      loaded: false
+      chartOptions: null
     }
   },
 
-  Mounted: function () {
-    async function DATA() {
-      // const data = await wait.json()
+  mounted: function () {
+    /* async function DATA() {
       const dataArray = ['HELLO']
       console.log(dataArray)
     }
+    DATA() */
 
-    this.chartData = {
+    let x = ['3', '4', '5']
+    ;(this.chartData = {
       labels: [
         'AL',
         'AK',
@@ -186,19 +94,14 @@ export default {
         {
           label: 'COMPLAINT CASES',
           backgroundColor: ['#E6F7FF'],
-          data: this.business_state
+          data: x
         }
       ]
-    }
-    this.chartOptions = {
-      responsive: true
-    }
+    }),
+      (this.chartOptions = {
+        responsive: true
+      })
     this.loaded = true
   }
 }
- onMounted(() => {
-  DATA()
-  //console.log(getData())
-}),
 </script>
- -->
