fea634a99821d9f96bff6f1d4593f5691f8d2ce9
AngelaW45 <113141296+AngelaW45@users.noreply.github.com>
2023-04-30 22:08:37 -0400

    9

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,21 +1,15 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-// import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <!-- <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header> -->
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/chart1">BarChart</RouterLink>
+      <RouterLink to="/chart2">PieChart</RouterLink>
+    </nav>
+  </header>
 
   <RouterView />
 </template>
